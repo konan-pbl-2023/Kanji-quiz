0c95df08cd3ed897f561097e750c03714de6a3ca
kae-2114 <144084888+kae-2114@users.noreply.github.com>
2023-11-10 17:07:18 +0900

    スタート画面の追加

--- app/src/main/java/com/example/prototypeapi22/ResultActivity.java
+++ app/src/main/java/com/example/prototypeapi22/ResultActivity.java
@@ -2,6 +2,8 @@ package com.example.prototypeapi22;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,7 @@ public class ResultActivity  extends AppCompatActivity {
         setContentView(R.layout.activity_result);
 
         TextView score = (TextView) findViewById(R.id.score);
+        Button startButton = (Button) findViewById(R.id.startButton);
 
         Intent intent = getIntent();
         boolean[] answers = intent.getBooleanArrayExtra("answers");
@@ -26,5 +29,12 @@ public class ResultActivity  extends AppCompatActivity {
             }
         }
         score.setText(String.valueOf(trueCount));
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),StartActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
