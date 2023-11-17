3cf97a3a91460e587abdfdb66c7d46ea9b898a90
kae-2114 <144084888+kae-2114@users.noreply.github.com>
2023-11-17 13:39:06 +0900

    難易度選択機能のプログラムを追加

--- app/src/main/java/com/example/prototypeapi22/StartActivity.java
+++ app/src/main/java/com/example/prototypeapi22/StartActivity.java
@@ -17,10 +17,28 @@ public class StartActivity  extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         Button startButton = (Button) findViewById(R.id.startbutton);
-
         startButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(),MainActivity.class);
+                intent.putExtra("difficulty", 0);
+                startActivity(intent);
+            }
+        });
+
+        Button startButton1 = (Button) findViewById(R.id.startbutton1);
+        startButton1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),MainActivity.class);
+                intent.putExtra("difficulty", 1);
+                startActivity(intent);
+            }
+        });
+
+        Button startButton2 = (Button) findViewById(R.id.startbutton2);
+        startButton2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),MainActivity.class);
+                intent.putExtra("difficulty", 2);
                 startActivity(intent);
             }
         });
